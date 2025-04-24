dd7668b032b49aa98bbd4d51a02758257b4ddef8
dlsdk0601 <dlsdk0601@gmail.com>
2025-04-25 02:20:45 +0900

    TEST: main test

--- was/was/application.py
+++ was/was/application.py
@@ -1,5 +1,6 @@
-from was.repository.upbit import get_account
+from was.model.coin import MarketType
+from was.repository.upbit import get_orders_chance, GetOrderChanceReq
 
 if __name__ == '__main__':
-    res = get_account()
-    print(f'{res=}')
+    res = get_orders_chance(GetOrderChanceReq(market=MarketType.KRW_BTC))
+    print(f'res={res}')
