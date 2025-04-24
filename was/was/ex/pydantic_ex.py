11b0209e667957484997fa11e1f6b9a89dc3f85c
inajung <ina.jung@coint.co.kr>
2025-04-24 20:11:12 +0900

    feat: pydantic BaseModel 추가

---
+++ was/was/ex/pydantic_ex.py
@@ -0,0 +1,12 @@
+import pydantic
+from stringcase import camelcase
+
+
+class BaseModel(pydantic.BaseModel):
+    class Config:
+        alias_generator = camelcase
+        populate_by_name = True
+        arbitrary_types_allowed = True
+        by_alias = True
+
+
