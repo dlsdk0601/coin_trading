1d1798b197c8b1e1b761935c27109a8d256b38cc
dlsdk0601 <dlsdk0601@gmail.com>
2025-05-07 21:09:41 +0900

    feat: layout 추가

---
+++ admin/src/app/(sf)/layout.tsx
@@ -0,0 +1,22 @@
+import { PropsWithChildren } from "react";
+import { HeaderView } from "../../view/layout/HeaderView";
+import LeftSidebarView from "../../view/layout/leftSidebarView";
+import { getUser } from "../../actions/session";
+
+const Layout = async (props: PropsWithChildren) => {
+  const user = await getUser();
+  console.log(user);
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <LeftSidebarView />
+      <div className="relative flex flex-1 flex-col overflow-x-hidden overflow-y-auto">
+        <HeaderView />
+        <main>
+          <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">{props.children}</div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
