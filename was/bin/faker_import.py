91f8518896088d5fa859583f39f9bc41dad21019
inajung <ina.jung@coint.co.kr>
2025-05-08 17:38:06 +0900

    feat: manager faker 추가

--- was/bin/faker_import.py
+++ was/bin/faker_import.py
@@ -3,11 +3,13 @@ from typing import Callable
 from was.application import app
 from was.model import db
 from was.model.config import Config
+from was.model.manager import Manager
 
 
 def main() -> None:
     importers: list[Callable[[], None]] = [
         _import_config,
+        _import_manager,
     ]
 
     for importer in importers:
@@ -24,6 +26,12 @@ def _import_config() -> None:
     db.session.add(c)
     db.session.commit()
 
+def _import_manager() -> None:
+    m = Manager()
+    m.id = 'test'
+    m.password_hash = Manager.hash_password('1234')
+    db.session.add(m)
+    db.session.commit()
 
 if __name__ == '__main__':
     main()
