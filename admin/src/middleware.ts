9202a13e0ebc72623a859e55dd4179ee7928f9cc
inajung <ina.jung@coint.co.kr>
2025-05-08 17:39:40 +0900

    feat: 이미지 외에 파비콘, svg도 예외에 추가

--- admin/src/middleware.ts
+++ admin/src/middleware.ts
@@ -23,5 +23,5 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.(?:png|ico|jpg|jpeg|svg)$).*)"],
 };
