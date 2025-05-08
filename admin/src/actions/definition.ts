fd5b8f7e2fb8c6972ccb077fa36a052e75ebc217
inajung <ina.jung@coint.co.kr>
2025-05-08 18:39:12 +0900

    feat: debug 에 따라서 유효성 검사 분기 처리

--- admin/src/actions/definition.ts
+++ admin/src/actions/definition.ts
@@ -1,22 +1,27 @@
 import { z } from "zod";
+import { config } from "../config";
+import { ResError } from "../api/schema";
 
 export const SignInFormSchema = z.object({
-  id: z.string().trim(),
-  password: z
-    .string()
-    .min(8, { message: "8 글자 이상 입력해주세요." })
-    .regex(/[a-zA-Z]/, { message: "대문자를 포함해야합니다." })
-    .regex(/[0-9]/, { message: "숫자를 포함해야합니다." })
-    .regex(/[^a-zA-Z0-9]/, { message: "특수 문자를 포함해야합니다." })
-    .trim(),
+  id: z.string().nonempty("id를 입력해주세요.").trim(),
+  password: config.debug
+    ? z.string().nonempty("비밀번호를 입력해주세요.").trim()
+    : z
+        .string()
+        .min(8, { message: "비밀번호를 8글자 이상 입력해주세요." })
+        .regex(/[a-zA-Z]/, { message: "비밀번호는 대문자를 포함해야합니다." })
+        .regex(/[0-9]/, { message: "비밀번호는 숫자를 포함해야합니다." })
+        .regex(/[^a-zA-Z0-9]/, { message: "비밀번호는 특수 문자를 포함해야합니다." })
+        .trim(),
 });
 
-export type FormState =
-  | {
-      errors?: {
-        id?: string[];
-        password?: string[];
-      };
-      message?: string;
-    }
-  | undefined;
+interface FormState<T> {
+  data?: T;
+  error?: string;
+}
+
+export type FormRes<T> = FormState<T> | undefined;
+
+export function isError(res: any | ResError): res is ResError {
+  return (res as ResError).error !== undefined;
+}
