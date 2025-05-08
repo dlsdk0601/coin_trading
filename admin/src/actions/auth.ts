9170375e7097b677d92f2f8daa0ecc10f4d18eff
inajung <ina.jung@coint.co.kr>
2025-05-08 18:40:06 +0900

    feat: 변경된 type에 맞게 수정

--- admin/src/actions/auth.ts
+++ admin/src/actions/auth.ts
@@ -1,29 +1,47 @@
 "use server";
 
 import { isNil } from "lodash";
-import { FormState, SignInFormSchema } from "./definition";
+import { FormRes, isError, SignInFormSchema } from "./definition";
 import { api } from "../api/api";
 import { createSession } from "./session";
+import { SignInRes } from "../api/schema";
 
-export async function signIn(state: FormState, formData: FormData) {
+export async function signIn(state: FormRes<SignInRes>, formData: FormData) {
   const validatedFields = SignInFormSchema.safeParse({
     id: formData.get("id"),
     password: formData.get("password"),
   });
 
   if (!validatedFields.success) {
-    return { errors: validatedFields.error.flatten().fieldErrors };
+    const error = getErrorMessage(validatedFields.error.flatten().fieldErrors);
+    return { error };
   }
 
   const { id, password } = validatedFields.data;
 
   const res = await api.signIn({ id, password });
 
-  if (isNil(res)) {
-    return { message: "API 통신 실패" };
+  if (isError(res)) {
+    return { error: res.error };
   }
 
   await createSession(res.token);
 
-  return { token: res.token };
+  return { data: { token: res.token } };
+}
+
+function getErrorMessage(error: Record<string, string[] | undefined>): string {
+  let message = "";
+
+  for (const key of Object.keys(error)) {
+    const v = error[key];
+    if (isNil(v)) {
+      continue;
+    }
+
+    message = v.at(0) ?? "";
+    break;
+  }
+
+  return message;
 }
