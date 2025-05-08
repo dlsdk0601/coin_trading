a260e0dd635c1abee76e2a9c8391e968d55e822d
inajung <ina.jung@coint.co.kr>
2025-05-08 17:35:41 +0900

    feat: ok, err 에 res_jsonify 추가

--- was/was/ex/api.py
+++ was/was/ex/api.py
@@ -1,5 +1,7 @@
 from enum import auto
-from typing import TypeVar, Generic, Any
+from typing import TypeVar, Generic, Any, Type
+
+from flask import request, Response, current_app
 
 from was.ex.enum_ex import StringEnum
 from was.ex.pydantic_ex import BaseModel
@@ -22,9 +24,19 @@ class Res(BaseModel, Generic[RES_DATA]):
     status: ResStatus
 
 
-def ok(data: RES_DATA) -> Res[RES_DATA]:
-    return Res(data=data, errors=[], validation_errors=[], status=ResStatus.OK)
+def res_jsonify(res: Res) -> Response:
+    return current_app.response_class(res.model_dump_json(by_alias=True))
+
+def ok(data: RES_DATA):
+    return res_jsonify(
+        Res(data=data, errors=[], validation_errors=[], status=ResStatus.OK)
+    )
 
 
-def err(*errors: str) -> Res[RES_DATA]:
-    return Res(errors=list(errors), validation_errors=[], status=ResStatus.OK)
+def err(*errors: str):
+    return res_jsonify(
+        Res(data=None, errors=list(errors), validation_errors=[], status=ResStatus.OK)
+    )
+
+def get_req(req_type: Type[BaseModel]):
+    return req_type.model_validate(request.get_json(force=True))
